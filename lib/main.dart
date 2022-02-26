8db0652f8cb8d51454930320e55228c90c54e3ac
Swastik133 <75462341+Swastik133@users.noreply.github.com>
2022-02-26 21:47:22 +0530

    day 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers
 
+import 'package:crash_course_8_hour/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,13 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to this app"),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
