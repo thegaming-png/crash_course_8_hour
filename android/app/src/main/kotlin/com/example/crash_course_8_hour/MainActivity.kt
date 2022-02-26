a0b2fab6cdcbcf34c2d377179c3f7fc117fb6c49
Swastik133 <75462341+Swastik133@users.noreply.github.com>
2022-02-26 21:43:07 +0530

    First Commit

---
+++ android/app/src/main/kotlin/com/example/crash_course_8_hour/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.crash_course_8_hour
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Center(
+          child: Container(
+            child: Text("Welcome to this app"),
+          ),
+        ),
+      ),
+    );
+  }
+}
